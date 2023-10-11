65038b4a42885943dfc004dfaeb16493af18a646
DevandaYudhaBharagus <devanda0308@gmail.com>
2023-10-11 21:52:09 +0700

    update

--- app.py
+++ app.py
@@ -17,9 +17,9 @@ db = client[DB_NAME]
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def home():
-   return render_template("index.html")
+   return render_template('index.html')
 
 @app.route("/movie", methods=["POST"])
 def movie_post():
@@ -29,7 +29,7 @@ def movie_post():
     comment_receive = request.form['comment_give']
 
     headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-    data = requests.get(url_receive,)
+    data = requests.get(url_receive, headers=headers)
 
     soup = BeautifulSoup(data.text, 'html.parser')
 
