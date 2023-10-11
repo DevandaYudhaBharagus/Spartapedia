3013ee26f9bd0847f54b3676929a493416d762fc
DevandaYudhaBharagus <devanda0308@gmail.com>
2023-10-11 20:31:54 +0700

    update

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import os
+import requests
+from bs4 import BeautifulSoup
 from os.path import join, dirname
 from dotenv import load_dotenv
 from flask import Flask, render_template, request, jsonify
